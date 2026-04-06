6982460423eda40f3cf9fdd822b499975b0e9f9c
kyoChiwow <kyoayasakicsgo@gmail.com>
2026-04-06 17:49:20 +0600

    Move on to toggle themeing
    Signed-off-by: kyoChiwow <kyoayasakicsgo@gmail.com>

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,7 +1,8 @@
 export default function About() {
     return (
-        <div>
+        <div className="py-16 px-4 container mx-auto">
             <h1>this is About component</h1>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ex incidunt ullam tempore quisquam exercitationem facere aliquid cum, iusto molestias neque consectetur consequatur nostrum totam nihil accusantium corrupti ducimus, excepturi error!</p>
         </div>
     )
 }
