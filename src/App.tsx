214d1a583502d2402af4da861f8617657359711c
kyoChiwow <kyoayasakicsgo@gmail.com>
2026-04-06 15:42:35 +0600

    Setup done and already added the basic components
    Signed-off-by: kyoChiwow <kyoayasakicsgo@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
-function App() {
+import { Outlet } from "react-router";
+import CommonLayout from "./components/layout/CommonLayout";
 
+function App() {
   return (
-    <>
-      <h1 className="">This is a simple react app</h1>
-    </>
-  )
+    <CommonLayout>
+      <Outlet />
+    </CommonLayout>
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/layout/AdminLayout.tsx
@@ -0,0 +1,10 @@
+import { Outlet } from "react-router";
+
+export default function AdminLayout() {
+  return (
+    <div>
+      AdminLayout
+      <Outlet />
+    </div>
+  );
+}
